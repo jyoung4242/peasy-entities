633103c7d77e9d94c4ddda9965dd92d0684c1c7c
Justin Young <justin_dean_young@yahoo.com>
2023-06-18 19:52:23 -0400

    should be working

--- src/components/gameobject.ts
+++ src/components/gameobject.ts
@@ -1,20 +1,30 @@
-import { Vector } from "@peasy-lib/peasy-physics";
+import { CollidingResolution, Entity, Intersection, Vector } from "@peasy-lib/peasy-physics";
 import { Shape } from "@peasy-lib/peasy-physics/dist/types/shape";
 
 export type GOconfig = {
   startingPosition: Vector;
   size: Vector;
-  shape: Shape;
-  color: string;
+  orientation: number;
+  color?: string;
+  maxSpeed?: number;
 };
 
 export class GameObject {
-  position = new Vector(0, 0);
-  size = new Vector(0, 0);
+  public position = new Vector(0, 0);
+  public size = new Vector(0, 0);
+  public shapes = <any>[];
+  public forces = [];
+  public mass: number = 1;
+  public restitution: number = 1;
+  public color?: string;
+  public maxSpeed?: number;
+  public colliding?: (entity: Entity, intersection: Intersection) => CollidingResolution;
+  public orientation: number;
 
   constructor(config: GOconfig) {
     this.position = config.startingPosition;
     this.size = config.size;
+    this.orientation = config.orientation;
   }
 
   static create(config: GOconfig) {
--- src/main.ts
+++ src/main.ts
@@ -6,43 +6,58 @@ import { Physics, Vector } from "@peasy-lib/peasy-physics";
 import { Lighting } from "@peasy-lib/peasy-lighting";
 import { GOconfig, GameObject } from "./components/gameobject";
 import { Engine } from "@peasy-lib/peasy-engine";
+import { Shape } from "@peasy-lib/peasy-physics/dist/types/shape";
 
 const model = {
-  objects: <GameObject>[],
+  objects: <any>[],
   canvas: <any>undefined,
 };
 const template = `
     <viewport-layer>
-        <object-layers \${obj<=*objects}></object-layers>
-        <canvas class="physics-canvas" \${==>canvas} width:"100%" height:"100%"></canvas>
+        <object-layers class="object" \${obj<=*objects} style="width: \${obj.size.x}px ;height: \${obj.size.y}px; transform: translate(\${obj.position.x}px,\${obj.position.y}px);"></object-layers>
+        <canvas class="physics-canvas" \${==>canvas} width="100%" height="100%"></canvas>
     </viewport-layer>
     
 `;
 
 const myView = UI.create(document.body, model, template);
 await myView.attached;
-console.log(model.canvas);
 
 Physics.initialize({
-  collisions: {
-    rect: ["circle", "stadium", "rect"],
-    circle: ["rect", "stadium", "circle"],
-    stadium: ["rect", "circle", "stadium"],
-  },
-  ctx: model.canvas?.getContext("2d"),
+  collisions: {},
+  ctx: model.canvas.getContext("2d"),
   showAreas: true,
 });
 
-Engine.create({ fps: 240, callback: updatePeasy });
+console.log(Physics);
+
+const myEngine = Engine.create({ fps: 240, callback: updatePeasy });
 
 function createObject() {
   let newObject: GameObject;
-  const myConfig: GOconfig = {};
+  const myConfig: GOconfig = {
+    startingPosition: new Vector(5, 5),
+    size: new Vector(16, 16),
+    color: "blue",
+    orientation: 0,
+  };
 
   newObject = GameObject.create(myConfig);
+  const shape = {
+    position: { x: 0, y: 0 },
+    radius: undefined,
+    size: new Vector(16, 16),
+    alignment: undefined,
+    types: ["rect"],
+  };
+  newObject.shapes = [shape];
+  Physics.addEntities([newObject]);
   model.objects.push(newObject);
 }
 
 function updatePeasy(deltaTime: number, now: number) {
   Physics.update(deltaTime, now);
 }
+
+createObject();
+myEngine.start();
